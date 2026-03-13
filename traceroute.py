267cad91d1804b79c55557c99e86f7b4b6b3a2a1
Pranav <Gonarchov@outlook.com>
2026-03-13 16:22:04 +0530

    Update traceroute.py
    Added code

--- traceroute.py
+++ traceroute.py
@@ -1,1 +1,39 @@
-placeholder
+import socket
+def traceroute(host):
+    dest = socket.gethostbyname(host)
+    max_hops = 30
+    port = 33434
+    print("\nTraceroute to", host)
+
+    for ttl in range(1, max_hops + 1):
+
+        recv_socket = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_RAW,
+            socket.IPPROTO_ICMP
+        )
+
+        send_socket = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_DGRAM,
+            socket.IPPROTO_UDP
+        )
+
+        send_socket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
+
+        recv_socket.bind(("", port))
+        recv_socket.settimeout(2)
+        send_socket.sendto(b"", (host, port))
+        addr = None
+        try:
+            data, addr = recv_socket.recvfrom(512)
+            print(ttl, addr[0])
+
+        except socket.timeout:
+            print(ttl, "*")
+
+        recv_socket.close()
+        send_socket.close()
+
+        if addr and addr[0] == dest:
+            break

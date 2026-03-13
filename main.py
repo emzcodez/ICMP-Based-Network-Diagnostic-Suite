df169928decbf1424f5363b41fe96931f3f438c1
Pranav <Gonarchov@outlook.com>
2026-03-13 16:21:08 +0530

    Update main.py
    Added code

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-placeholder
+import sys
+from icmp_ping import ping
+from stats import compute_stats
+from traceroute import traceroute
+
+def run_ping(host):
+    print("\nPinging", host)
+    results = []
+    for i in range(4):
+
+        rtt = ping(host)
+
+        if rtt:
+            print("Reply:", round(rtt,2), "ms")
+        else:
+            print("Request timed out")
+
+        results.append(rtt)
+
+    stats = compute_stats(results)
+
+    print("\nStatistics")
+    print("Sent:", stats["sent"])
+    print("Received:", stats["received"])
+    print("Packet Loss:", stats["loss"], "%")
+    print("Average RTT:", stats["avg_rtt"], "ms")
+
+    traceroute(host)
+
+if __name__ == "__main__":
+
+    hosts = sys.argv[1:]
+
+    for host in hosts:
+        run_ping(host)

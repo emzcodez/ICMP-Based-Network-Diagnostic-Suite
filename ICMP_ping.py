a2c7d593a884a0c834840a157fb6b5b2252f1392
Pranav <Gonarchov@outlook.com>
2026-03-16 09:05:09 +0530

    Update ICMP_ping.py

--- ICMP_ping.py
+++ ICMP_ping.py
@@ -10,21 +10,22 @@ SECRET_KEY = b"my_super_secret_key"
 
 def checksum(source_string):
     sum = 0
-    count_to = (len(source_string) // 2) * 2
-    count = 0
+    count_to = (len(source_string) // 2) * 2  #Ensures data is processed two bytes at a time.
+    count = 0  #Current index while looping through the bytes
     while count < count_to:
-        this_val = source_string[count + 1] * 256 + source_string[count]
+        this_val = source_string[count + 1] * 256 + source_string[count]    #Shifting the second value 8bits to the left so that it occupies the 16bit space properly
         sum = sum + this_val
         sum = sum & 0xffffffff
         count = count + 2
-    if count_to < len(source_string):
+    if count_to < len(source_string):   #if pkt was odd length, one byte will be left at the end (since data is processed two bytes at a time)
         sum = sum + source_string[len(source_string) - 1]
         sum = sum & 0xffffffff
+    #Adding carry: first value is upper 16b (carry) and second is lower 16b (the actual checksum)
     sum = (sum >> 16) + (sum & 0xffff)
     sum = sum + (sum >> 16)
     answer = ~sum
     answer = answer & 0xffff
-    answer = answer >> 8 | (answer << 8 & 0xff00)
+    answer = answer >> 8 | (answer << 8 & 0xff00)   #swapping bytes to fit Big Endian format
     return answer
 
 def create_packet(pid):
@@ -45,6 +46,7 @@ def create_packet(pid):
 def ping(host):
     try:
         dest = socket.gethostbyname(host)
+        #AF_INET = IPv4 ; SOCK_RAW = raw packet access (access data layer directly) ; IPPROTO_ICMP = ICM protocol
         sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
     except (PermissionError, socket.gaierror):
         return None
@@ -53,29 +55,30 @@ def ping(host):
         pid = os.getpid() & 0xFFFF
         packet = create_packet(pid)
         sock.settimeout(2)
-        start = time.time()
-        sock.sendto(packet, (dest, 0))
+        start = time.time() #Start the two second timeout
+        sock.sendto(packet, (dest, 0))  #Send the packet to dest
 
         while True:
             data, addr = sock.recvfrom(1024)
             
-            # ICMP header starts after the IP header (usually 20 bytes)
+            #IP header comes first, then ICMP header, then finally the payload (response data)
             ip_header_len = (data[0] & 0x0F) * 4
             icmp_header = data[ip_header_len:ip_header_len + 8]
             payload = data[ip_header_len + 8:]
 
-            # Unpack using Network Byte Order '!'
+            # Unpack using the Network Byte Order '!' (Big Endian)
+            #Refer earlier comment for the header format
             icmp_type, code, chk, packet_id, seq = struct.unpack("!bbHHh", icmp_header)
 
-            # Type 0 is Echo Reply. ID must match our PID.
+            #Type 0 is Echo Reply and ID must match our PID
             if icmp_type == 0 and packet_id == pid:
                 timestamp_data = payload[:8]
                 received_sig = payload[8:40]
                 expected_sig = hmac.new(SECRET_KEY, timestamp_data, hashlib.sha256).digest()
 
-                if hmac.compare_digest(received_sig, expected_sig):
+                if hmac.compare_digest(received_sig, expected_sig): #If the HMAC signatures match, calculate RTT (in ms)
                     return (time.time() - start) * 1000
-    except socket.timeout:
+    except socket.timeout:  #Took longer than 2s: packet is lost
         return None
     finally:
         sock.close()

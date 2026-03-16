76eaacd259c197e3f45a000afaaa5e4fe35c0fe2
Pranav <Gonarchov@outlook.com>
2026-03-16 10:24:55 +0530

    Update main.py
    Added min-max RTT functionality

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import sys
+import socket
+import time
 from icmp_ping import ping
 from stats import compute_stats
 from traceroute import traceroute
 
 def run_ping(host):
-    print("\nPinging", host)
+    try:    #in case user enters a bad hostname (which cant be resolved)
+        dest = socket.gethostbyname(host)
+    except socket.gaierror:
+        print("Invalid host")
+        return
+    print(f"\nPinging {host} [{dest}]")   #Resolving hostname -> ip
     results = []
     for i in range(4):
 
@@ -16,6 +23,7 @@ def run_ping(host):
             print("Request timed out")
 
         results.append(rtt)
+        time.sleep(1)   #waiting for a while between pings
 
     stats = compute_stats(results)
 
@@ -24,10 +32,15 @@ def run_ping(host):
     print("Received:", stats["received"])
     print("Packet Loss:", stats["loss"], "%")
     print("Average RTT:", stats["avg_rtt"], "ms")
+    print("Min RTT:", stats["min_rtt"], "ms")
+    print("Max RTT:", stats["max_rtt"], "ms")
 
     traceroute(host)
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:   #making sure user runs program w arguments
+        print("Usage: python3 main.py <host1> <host2> ...")
+        sys.exit(1)
 
     hosts = sys.argv[1:]
 

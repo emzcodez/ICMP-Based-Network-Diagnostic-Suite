92e31452ba7a9a0aa5d32b1713bb9764b18f74ad
Pranav <Gonarchov@outlook.com>
2026-03-16 10:24:16 +0530

    Update stats.py
    Added min-max functionality

--- stats.py
+++ stats.py
@@ -8,10 +8,14 @@ def compute_stats(results):
     rtts = [r for r in results if r]
 
     avg = sum(rtts) / len(rtts) if rtts else 0
+    min_rtt = min(rtts) if rtts else 0
+    max_rtt = max(rtts) if rtts else 0
 
     return {
         "sent": sent,
         "received": received,
         "loss": loss,
-        "avg_rtt": avg
+        "avg_rtt": avg,
+        "min_rtt": min_rtt,
+        "max_rtt": max_rtt
     }
